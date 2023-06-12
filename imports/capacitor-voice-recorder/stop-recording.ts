63944a4f36892ec6fa9ce99c13971d408ebe648c
flakeed <vova.krivosheya.2004@gmail.com>
2023-06-12 18:29:06 +0000

    Clear main

--- imports/capacitor-voice-recorder/stop-recording.ts
+++
@@ -1,9 +0,0 @@
-import { DeepClient } from "@deep-foundation/deeplinks/imports/client";
-import { VoiceRecorder } from "capacitor-voice-recorder"
-import uploadRecords from "./upload-records";
-
-export default async function stopRecording(deep: DeepClient, containerLinkId, startTime) {
-    const { value: record } = await VoiceRecorder.stopRecording();
-    console.log("record",record)
-    return record;
-}
